fcf69c64f1ae87a3df4932de7b7140c4da50a58e
sayeems <sayeems@gmail.com>
2023-06-26 20:17:29 +0600

    exception handler added

---
+++ src/app/error.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Image from "next/image";
+import serverError from "../../public/server-error.png";
+export default function ServerError() {
+  return (
+    <div
+      className="flex items-center justify-center flex-col"
+      style={{ minHeight: "calc(100vh - 200px)" }}
+    >
+      <h2 className="font-bold text-black text-xl text-center max-w-4xl">
+        Oh no, it seems our servers have taken a coffee break. We&apos;re
+        working on waking them up from their caffeine-induced slumber. Hang
+        tight!
+      </h2>
+      <Image
+        style={{ maxWidth: "50%", height: "auto" }}
+        src={serverError}
+        alt="not found"
+        width={300}
+        height={300}
+        layout="responsive"
+        placeholder="blur"
+      ></Image>
+    </div>
+  );
+}
---
+++ src/app/not-found.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import notFoundImage from "../../public/not-found.png";
+export default function NotFound() {
+  return (
+    <div
+      className="flex items-center justify-center flex-col"
+      style={{ minHeight: "calc(100vh - 200px)" }}
+    >
+      <h2 className="font-bold text-black text-xl text-center max-w-4xl">
+        Oops! Looks like we took a wrong turn in the digital wilderness. The
+        page you&apos;re looking for seems to be on an unexpected vacation.
+        We&apos;ll send out a search party to bring it back!
+      </h2>
+      <Image
+        style={{ maxWidth: "50%", height: "auto" }}
+        src={notFoundImage}
+        alt="not found"
+        width={300}
+        height={300}
+        layout="responsive"
+        placeholder="blur"
+      ></Image>
+    </div>
+  );
+}
