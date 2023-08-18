37d78ec6d33276a66538a302ee2cbc5536b182c4
sayeems <sayeems@gmail.com>
2023-08-18 16:16:02 +0600

    locale added

--- src/app/[locale]/[slug]/not-found.jsx
+++ src/app/[locale]/[slug]/not-found.jsx
@@ -1,21 +1,19 @@
-"use client";
-
 import Image from "next/image";
-import serverError from "../../public/server-error.png";
-export default function ServerError() {
+import notFoundImage from "../../../../public/not-found.png";
+export default function NotFound() {
   return (
     <div
       className="flex items-center justify-center flex-col"
       style={{ minHeight: "calc(100vh - 200px)" }}
     >
       <h2 className="font-bold text-black text-xl text-center max-w-4xl">
-        Oh no, it seems our servers have taken a coffee break. We&apos;re
-        working on waking them up from their caffeine-induced slumber. Hang
-        tight!
+        Oops! Looks like we took a wrong turn in the digital wilderness. The
+        page you&apos;re looking for seems to be on an unexpected vacation.
+        We&apos;ll send out a search party to bring it back!
       </h2>
       <Image
         style={{ maxWidth: "50%", height: "auto" }}
-        src={serverError}
+        src={notFoundImage}
         alt="not found"
         width={300}
         height={300}
--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,37 @@
-import { NextResponse } from "next/server";
-import { sampleData } from "./sample";
+// import { NextResponse } from "next/server";
+// import { sampleData } from "./sample";
 
-export async function middleware(request) {
-  let response = NextResponse.next();
+// export async function middleware(request) {
+//   let response = NextResponse.next();
 
-  // let head = process.env.skey;
-  const head = sampleData.sample;
+//   // let head = process.env.skey;
+//   const head = sampleData.sample;
 
-  response.headers.set("x-test-header", head);
-  response.headers.set(
-    "cache-control",
-    "public, max-age=604800, must-revalidate"
-  );
+//   response.headers.set("x-test-header", head);
+//   response.headers.set(
+//     "cache-control",
+//     "public, max-age=604800, must-revalidate"
+//   );
 
-  return response;
-}
+//   return response;
+// }
+
+// export const config = {
+//   matcher: ["/"],
+// };
+import createMiddleware from "next-intl/middleware";
+
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ["en", "fr", "de", "it", "ja"],
+
+  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
+  defaultLocale: "en",
+  localeDetection: false,
+});
 
 export const config = {
-  matcher: ["/"],
+  // Skip all paths that should not be internationalized. This example skips the
+  // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
