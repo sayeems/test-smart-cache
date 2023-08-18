4d0f8259dfbbc462c6a58c933725ed733bb3a5b5
sayeems <sayeems@gmail.com>
2023-08-18 18:49:17 +0600

    node version 16

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,3 @@
-// import { NextResponse } from "next/server";
-// import { sampleData } from "./sample";
-
-// export async function middleware(request) {
-//   let response = NextResponse.next();
-
-//   // let head = process.env.skey;
-//   const head = sampleData.sample;
-
-//   response.headers.set("x-test-header", head);
-//   response.headers.set(
-//     "cache-control",
-//     "public, max-age=604800, must-revalidate"
-//   );
-
-//   return response;
-// }
-
-// export const config = {
-//   matcher: ["/"],
-// };
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
